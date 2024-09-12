c8614566eebde05e797a5772413fe4dedc625a56
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-09-12 15:03:57 +0200

    comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
@@ -15,8 +16,38 @@ class Comment
   )]
   private ?int $id = null;
 
+  #[ORM\Column(length: 2500)]
+  private ?string $commentMessage = null;
+
+  #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+  private ?\DateTimeInterface $CommentDateCreated = null;
+
   public function getId(): ?int
   {
     return $this->id;
+  }
+
+  public function getCommentMessage(): ?string
+  {
+      return $this->commentMessage;
+  }
+
+  public function setCommentMessage(string $commentMessage): static
+  {
+      $this->commentMessage = $commentMessage;
+
+      return $this;
+  }
+
+  public function getCommentDateCreated(): ?\DateTimeInterface
+  {
+      return $this->CommentDateCreated;
+  }
+
+  public function setCommentDateCreated(\DateTimeInterface $CommentDateCreated): static
+  {
+      $this->CommentDateCreated = $CommentDateCreated;
+
+      return $this;
   }
 }
