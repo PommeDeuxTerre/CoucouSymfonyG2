7c17be91cf25386bb9d49cf7dde6f66e8db6a47a
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-09-20 13:17:07 +0200

    save

--- migrations/Version20240918123926.php
+++ migrations/Version20240918123926.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240912121728 extends AbstractMigration
+final class Version20240918123926 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20240912121728 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post ADD post_title VARCHAR(255) NOT NULL, ADD post_description LONGTEXT NOT NULL, ADD post_date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD post_date_published DATETIME DEFAULT NULL, ADD post_is_published TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE post ADD user_id INT UNSIGNED NOT NULL');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DA76ED395 ON post (user_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post DROP post_title, DROP post_description, DROP post_date_created, DROP post_date_published, DROP post_is_published');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DA76ED395');
+        $this->addSql('DROP INDEX IDX_5A8A6C8DA76ED395 ON post');
+        $this->addSql('ALTER TABLE post DROP user_id');
     }
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -9,50 +9,83 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
 {
-  #[ORM\Id]
-  #[ORM\GeneratedValue]
-  #[ORM\Column(
-    options: ["unsigned" => true]
-  )]
-  private ?int $id = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(
+        options: ["unsigned" => true]
+    )]
+    private ?int $id = null;
 
-  #[ORM\Column(length: 2500)]
-  private ?string $commentMessage = null;
+    #[ORM\Column(length: 2500)]
+    private ?string $commentMessage = null;
 
-  #[ORM\Column(
-    type: Types::DATETIME_MUTABLE,
-    options: [
-      'default' => 'CURRENT_TIMESTAMP',
-    ],
-  )]
-  private ?\DateTimeInterface $CommentDateCreated = null;
+    #[ORM\Column(
+        type: Types::DATETIME_MUTABLE,
+        options: [
+        'default' => 'CURRENT_TIMESTAMP',
+        ],
+    )]
+    private ?\DateTimeInterface $CommentDateCreated = null;
 
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Post $post = null;
 
-  public function getCommentMessage(): ?string
-  {
-      return $this->commentMessage;
-  }
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
 
-  public function setCommentMessage(string $commentMessage): static
-  {
-      $this->commentMessage = $commentMessage;
 
-      return $this;
-  }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-  public function getCommentDateCreated(): ?\DateTimeInterface
-  {
-      return $this->CommentDateCreated;
-  }
+    public function getCommentMessage(): ?string
+    {
+        return $this->commentMessage;
+    }
 
-  public function setCommentDateCreated(\DateTimeInterface $CommentDateCreated): static
-  {
-      $this->CommentDateCreated = $CommentDateCreated;
+    public function setCommentMessage(string $commentMessage): static
+    {
+        $this->commentMessage = $commentMessage;
 
-      return $this;
-  }
+        return $this;
+    }
+
+    public function getCommentDateCreated(): ?\DateTimeInterface
+    {
+        return $this->CommentDateCreated;
+    }
+
+    public function setCommentDateCreated(\DateTimeInterface $CommentDateCreated): static
+    {
+        $this->CommentDateCreated = $CommentDateCreated;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Post $post): static
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
 }
